a81a4d1862e2c589ed997fec9947179204686366
rsnirob <47166768+rsnirob@users.noreply.github.com>
2022-06-02 12:55:12 +0600

    Create 01 creating variables.php

---
+++ Learn PHP/006 PHP Variables/01 creating variables.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$txt = "Hello world!";
+$x = 5;
+$y = 10.5;
+
+echo $txt;
+echo "<br>";
+echo $x;
+echo "<br>";
+echo $y;
+
+
+echo "<br><br><br>$txt <br> $x <br> $y";
+
+?>
+
+</body>
+</html>
